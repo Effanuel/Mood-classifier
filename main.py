0d25d854f356397640261ce536664c20a130c640
Juozas Rimantas <32594043+Effanuel@users.noreply.github.com>
2021-03-31 23:59:07 +0300

    Added initial cleaning data implementation

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import nltk
+import pandas as pd
+import re
+
+def readRawData(filePath: str ='./data.csv'):
+    return pd.read_csv(filePath, index_col=0)["0"]
+
+def cleanData(row):
+    matchAtUser = r"@\w+|#|:"
+    cleaned1 = re.sub(matchAtUser, "",  row).strip()
+
+    matchNonCharacters = r"(?!\s|\.|,)\W+"
+    cleaned2 = re.sub(matchNonCharacters, '', cleaned1)
+
+    matchDotsAndCommas = r"\.|,"
+    cleaned3 = re.sub(matchDotsAndCommas, " ", cleaned2).lower()
+
+    return cleaned3
+
 def main() -> None:
-    print('Hello Mood')
+    data = readRawData()
+
+    for i in range(len(data)):
+        print(i, cleanData(data[i]))
 
 if __name__ == "__main__":
     main()
