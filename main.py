0f609395cc88cda9d6cd205d4a9a90ba192fa5ec
Juozas Rimantas <32594043+Effanuel@users.noreply.github.com>
2021-04-15 21:20:42 +0300

    Using built in tweet samples, updated data cleaning func

--- main.py
+++ main.py
@@ -2,40 +2,50 @@ import nltk
 import pandas as pd
 import re
 import nltk
+import string
+from nltk.tag import pos_tag
 from nltk.corpus import stopwords
+from nltk.corpus import twitter_samples
+from nltk.stem.wordnet import WordNetLemmatizer
+
 
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('twitter_samples')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
-stopWords =  stopwords.words('english')
+stop_words = stopwords.words('english')
 
-def removeStopWords(text: str) -> str:
-    tokens = nltk.word_tokenize(text)
-    return " ".join([token for token in tokens if token not in stopWords])
 
-def readRawData(filePath: str ='./data.csv'):
-    return pd.read_csv(filePath, index_col=0)["0"]
+def cleanData(tokens):
+    lemmatizer = WordNetLemmatizer()
+    cleaned_tokens = []
+    for token, tag in pos_tag(tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
+        token = re.sub("(@[A-Za-z0-9_]+)","", token)
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
 
-def cleanData(row):
-    # TODO: remove links before this step
-    matchAtUser = r"@\w+|#|:"
-    cleaned1 = re.sub(matchAtUser, "",  row).strip()
-
-    matchNonCharacters = r"(?!\s|\.|,)\W+"
-    cleaned2 = re.sub(matchNonCharacters, '', cleaned1)
-
-    matchDotsAndCommas = r"\.|,"
-    cleaned3 = re.sub(matchDotsAndCommas, " ", cleaned2).lower()
-
-    return cleaned3
+        token = lemmatizer.lemmatize(token, pos)
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
 
 def main() -> None:
-    data = readRawData()
+    tokensPositive = twitter_samples.tokenized('positive_tweets.json')
+    tokensNegative = twitter_samples.tokenized('negative_tweets.json')
 
-    for i in range(len(data)):
-        cleaned = cleanData(data[i])
-        withoutStopWords = removeStopWords(cleaned)
-        print(i, withoutStopWords)
+    print(tokensPositive[0])
+    print(cleanData(tokens)(tokensPositive[0]))
+
+    print(tokensNegative[0])
+    print(cleanData(tokens)(tokensNegative[0]))
+
 
 if __name__ == "__main__":
     main()
