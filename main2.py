74717b3530b97a6632d42f6ed4b21c721ec55f56
Juozas Rimantas <32594043+Effanuel@users.noreply.github.com>
2021-04-18 22:06:27 +0300

    Updated code to use different data

--- main2.py
+++ main2.py
@@ -21,12 +21,14 @@ from collections import OrderedDict
 from sklearn.metrics import f1_score
 from colorama import Fore, Back, Style
 import matplotlib.pyplot as plt
+from nltk.tokenize import TweetTokenizer
 
 stop_words = stopwords.words('english')
 
-def cleanData(tweet_tokens, stop_words = ()):
-    lemmatizer = WordNetLemmatizer()
-    cleaned_tokens = []
+lemmatizer = WordNetLemmatizer()
+
+def __cleanData(tweet_tokens):
+    cleaned_tokens = ""
     for token, tag in pos_tag(tweet_tokens):
         token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
         token = re.sub("(@[A-Za-z0-9_]+)","", token)
@@ -38,9 +40,9 @@ def cleanData(tweet_tokens, stop_words = ()):
             pos = 'a'
 
         token = lemmatizer.lemmatize(token, pos)
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return " ".join(cleaned_tokens)
+        if len(token) > 0 and token not in string.punctuation:
+            cleaned_tokens += token.lower() + " "
+    return cleaned_tokens
 
 
 class Classifier:
@@ -48,15 +50,14 @@ class Classifier:
         self.train_data = []
         self.val_data = []
         self.model = None
-        self.tfidf_vc = TfidfVectorizer(min_df = 10, max_features = 10000, analyzer = "word", ngram_range = (1, 2), lowercase = True)
+        self.tfidf_vc = TfidfVectorizer(min_df = 10, max_features = 200000, analyzer = "word", ngram_range = (1, 2), lowercase = True)
 
 
     def loadData(self, test_size = 0.15):
-        positive_cleaned_tokens_list = [cleanData(tokens, stop_words) for tokens in twitter_samples.tokenized('positive_tweets.json')]
-        negative_cleaned_tokens_list = [cleanData(tokens, stop_words) for tokens in twitter_samples.tokenized('negative_tweets.json')]
+        data_ = pd.read_csv('cleaned_data.csv')
         data = pd.DataFrame(data={
-            'texts': positive_cleaned_tokens_list + negative_cleaned_tokens_list,
-            'labels': [1] * len(positive_cleaned_tokens_list) + [0] * len(negative_cleaned_tokens_list)
+            'texts': data_.text.astype('U'),
+            'labels': [0] * 800000 + [1] * 800000
             })
         train_data, val_data = train_test_split(data, test_size=test_size, random_state=777)
 
@@ -66,7 +67,7 @@ class Classifier:
     
     def fit(self):
         train_vc = self.tfidf_vc.fit_transform(self.train_data.texts)
-        self.model = LogisticRegression(C = 0.5, solver = "sag").fit(train_vc, self.train_data.labels)
+        self.model = LogisticRegression(C = 1.0, solver = "sag").fit(train_vc, self.train_data.labels)
         return self
 
     def print_score(self):
@@ -99,11 +100,12 @@ class Classifier:
             with open("Output.html", "w") as text_file:
                 text_file.write(exp.as_html())
 
-
-
 def main():
-    model = Classifier()
-    model = model.loadData().fit().print_score().predict(2, output_to_html=True)
+    model = Classifier()                    # Instantiate the classifier
+    model.loadData()                        # read data from file
+    model.fit()                             # fit logistic regression model with train data
+    model.print_score()                     # print model f1 accuracy based on test data
+    model.predict(2, output_to_html=True)   # predict label on text
 
 
 if __name__ == '__main__':
